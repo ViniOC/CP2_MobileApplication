b92d2eb5eb8ed4f51b55e3c1d6d0e3ff843ba093
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-04-05 22:38:05 -0300

    feat: create mock locations

---
+++ src/mocks/MockLocations.js
@@ -0,0 +1,79 @@
+import roupas from '../../assets/camiseta.png';
+import servico from '../../assets/servicos-voluntarios.png';
+import higiene from '../../assets/produtos-de-higiene-pessoal.png';
+import alimento from '../../assets/alimentos.png';
+
+const mockLocations = [
+    {
+      "id": 1, 
+      "latitude": -23.55914373, 
+      "longitude": -46.6573154, 
+      "title": "Precisando de Roupas", 
+      "image": roupas
+    },
+    {
+      "id": 2, 
+      "latitude": -23.56227741, 
+      "longitude": -46.64818101, 
+      "title": "Precisando de Servicos Voluntarios", 
+      "image": servico
+    },
+    {
+      "id": 3, 
+      "latitude": -23.56437876, 
+      "longitude": -46.65192318, 
+      "title": "Precisando de Produtos de Higiene", 
+      "image": higiene
+    },
+    {
+      "id": 4, 
+      "latitude": -23.56167005, 
+      "longitude": -46.65578963, 
+      "title": "Precisando de Roupas", 
+      "image": roupas
+    },
+    {
+      "id": 5, 
+      "latitude": -23.56517848, 
+      "longitude": -46.65407438, 
+      "title": "Precisando de Alimentos", 
+      "image": alimento
+    },
+    {
+      "id": 6, 
+      "latitude": -23.5673821, 
+      "longitude": -46.65358657, 
+      "title": "Precisando de Servicos Voluntarios", 
+      "image": servico
+    },
+    {
+      "id": 7, 
+      "latitude": -23.56843511, 
+      "longitude": -46.65227321, 
+      "title": "Precisando de Roupas", 
+      "image": roupas
+    },
+    {
+      "id": 8, 
+      "latitude": -23.56639418, 
+      "longitude": -46.64854244, 
+      "title": "Precisando de Roupas", 
+      "image": roupas
+    },
+    {
+      "id": 9, 
+      "latitude": -23.5656817, 
+      "longitude": -46.65348167, 
+      "title": "Precisando de Servicos Voluntarios", 
+      "image": servico
+    },
+    {
+      "id": 10, 
+      "latitude": -23.56311382, 
+      "longitude": -46.65349981, 
+      "title": "Precisando de Alimentos", 
+      "image": alimento
+    },
+]
+
+export default mockLocations;
