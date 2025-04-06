87816c293464d27216e5691d4dabf96c5f4cf24d
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-04-06 00:12:12 -0300

    refactor: remove Text tag with 'Header' text and update styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,7 @@ export default function Header(){
     return(
         <View style={styles.container}>
             <Image style = {styles.image} source = {require('../Image/header.png')}/>
-            <Text>Solidarize</Text>
-            <Text>Header</Text>
+            <Text style={styles.text}>Solidarize</Text>
         </View>
     )
 }
@@ -15,9 +14,10 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
         backgroundColor: "#e01d5e",
-        justifyContent: "space-between",
+        justifyContent: "center",
+        gap: 20,
         alignItems: "center",
-        padding: 10,
+        paddingVertical: 10,
         width: "100%",
         marginTop: 30,
 
@@ -25,5 +25,12 @@ const styles = StyleSheet.create({
     image: {
         width: 40,
         height: 40,
+    },
+    text: {
+        fontSize: 20,
+        color: "white",
+        fontWeight: "bold",
+        fontFamily: "Roboto",
+        textTransform: "uppercase",
     },
 });
