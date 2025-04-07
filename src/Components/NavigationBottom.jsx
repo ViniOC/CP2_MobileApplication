969cb50a8cd3d1b2bfd35f3e80a7faa21352d963
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-04-07 12:41:48 -0300

    feat: add NavigationBottom component for navigation functionality

---
+++ src/Components/NavigationBottom.jsx
@@ -0,0 +1,24 @@
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+
+export default function NavigationBottom({nameBottom, navigation, namePage}) {
+  return(
+    <TouchableOpacity style={styles.bottom} onPress={() => navigation.navigate(namePage)}>
+        <Text style={styles.textBottom}>{nameBottom}</Text>
+     </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+    bottom: {
+        borderRadius: 8,
+        padding: 10,
+        borderWidth: 1,
+        marginBottom: 10,
+        backgroundColor: "#ece9f6"
+    },
+    textBottom: {
+        fontSize: 16,
+        textAlign: "center",
+        color: "black"
+    },
+})
